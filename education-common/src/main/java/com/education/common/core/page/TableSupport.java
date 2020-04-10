38c554a5372b67be8a320ceac5a052bc2e55402b
a183678497 <545161585@qq.com>
2020-04-10 10:32:50 +0800

    初始化git项目

---
+++ education-common/src/main/java/com/education/common/core/page/TableSupport.java
@@ -0,0 +1,30 @@
+package com.education.common.core.page;
+
+import com.education.common.constant.Constants;
+import com.education.common.utils.ServletUtils;
+
+/**
+ * 表格数据处理
+ * 
+ * @author education
+ */
+public class TableSupport
+{
+    /**
+     * 封装分页对象
+     */
+    public static PageDomain getPageDomain()
+    {
+        PageDomain pageDomain = new PageDomain();
+        pageDomain.setPageNum(ServletUtils.getParameterToInt(Constants.PAGE_NUM));
+        pageDomain.setPageSize(ServletUtils.getParameterToInt(Constants.PAGE_SIZE));
+        pageDomain.setOrderByColumn(ServletUtils.getParameter(Constants.ORDER_BY_COLUMN));
+        pageDomain.setIsAsc(ServletUtils.getParameter(Constants.IS_ASC));
+        return pageDomain;
+    }
+
+    public static PageDomain buildPageRequest()
+    {
+        return getPageDomain();
+    }
+}
---
+++ education-common/src/main/java/com/education/common/enums/BusinessStatus.java
@@ -0,0 +1,19 @@
+package com.education.common.enums;
+
+/**
+ * 操作状态
+ * 
+ * @author education
+ */
+public enum BusinessStatus
+{
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
